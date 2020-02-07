cc0ef59144506ebf901f00c3ae53748d7b02f105
Heinen <jurnheinen111@gmail.com>
2020-02-07 23:38:08 +0100

    change folder structure

---
+++ src/isotherms.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+isotherm_models = ['langmuir', 'freundlich'] 
+
+def langmuir(p,params):
+	n = len(p)
+	langmuir = np.zeros(n)
+	for i in range(n):
+		langmuir[i] = params[i][0]*params[i][1]*p[i]/(1+(params[i][1]*p[i]))
+	return langmuir 
+
+def langmuirFit(x,qsat,b):
+	return qsat*x*b/(1+b*x)
